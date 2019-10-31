84ae4a7fa8bf615773ae80081489df683846f14e
Bryan Pedroza <bryanp@healthendeavors.com>
2019-10-31 10:41:08 -0700

    change order of setup call

--- tests/PatientProofTest.php
+++ tests/PatientProofTest.php
@@ -16,9 +16,9 @@ class PatientProofTest extends TestCase
 {
     public function setUp()
     {
-        MaxMD::Login(getenv("MAXMD_APIUSERNAME"),getenv("MAXMD_APIPASSWORD"));
-
         parent::setUp();
+
+        MaxMD::Login(getenv("MAXMD_APIUSERNAME"),getenv("MAXMD_APIPASSWORD"));
     }
 
     public function testCheckingPerson()
